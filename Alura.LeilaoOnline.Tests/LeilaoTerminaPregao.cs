06715113913a8e6e3cd8959a3794e4b5ce74f5e7
IgorAkiraKimura <77406454+IgorAkiraKimura@users.noreply.github.com>
2021-03-24 21:00:39 -0300

    04 - 01

--- Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -42,6 +42,25 @@ namespace Alura.LeilaoOnline.Tests
         }
 
         [Fact]
+        public void LancaInvalidOperationExceptionDadoPregaoNaoIniciado()
+        {
+            //Arrange
+            var leilao = new Leilao("Van Gogh");
+
+            try
+            {
+                //Act
+                leilao.TerminaPregao();
+                Assert.True(false);
+            }
+            catch (Exception e)
+            {
+                //Assert
+                Assert.IsType<System.InvalidOperationException>(e);
+            }
+        }
+
+        [Fact]
         public void RetornaZeroDadoLeilaoSemLance()
         {
             //Arrange
